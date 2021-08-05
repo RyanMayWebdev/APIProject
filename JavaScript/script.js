b6cf35ae4ece80adb77290dfabc9982e60b8aa76
Ryan May <ryan777may@gmail.com>
2021-08-05 17:34:45 -0400

    add ability to get only new releases

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -4,12 +4,19 @@ app.getGenre = ()=>{
    $('form').on('submit',(e)=>{
        e.preventDefault()
        let genre = $('#movie-genre').val()
-       app.apiCall(genre)
+       let newRelease = $('#newRelease').prop('checked')
+       console.log(newRelease)
+       app.apiCall(genre,newRelease)
    })
 }
 
-app.apiCall = (genre) =>{
-
+app.apiCall = (genre,recent) =>{
+    let releaseYear = ''
+    if (recent == true){
+        let year = new Date()
+        releaseYear = year.getFullYear()
+    }
+    console.log(releaseYear)
     $.ajax({
         url: `https://api.themoviedb.org/3/discover/movie?api_key=31beca076158503c129b3c1228e56ad2`,
                     method : 'GET',
@@ -18,7 +25,8 @@ app.apiCall = (genre) =>{
                         with_original_language : 'en',
                         'vote_count.gte' : 100,
                         sort_by : 'vote_average.desc',
-                        with_genres: genre
+                        with_genres: genre,
+                        primary_release_year : releaseYear
                     }
         
     }).then((res)=>{
